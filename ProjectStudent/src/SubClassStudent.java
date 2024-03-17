cabebec5438663047f4317b857759440317a2e85
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-03-17 17:13:27 -0300

    code added - SubClassStudent

---
+++ ProjectStudent/src/SubClassStudent.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Fujoshi Shouter
+ */
+public class SubClassStudent {
+    private double average;
+    private double a1;
+    private double a2;
+    
+    //Constructor
+    public SubClassStudent(){
+       average = 0; 
+    }
+    
+    //Acess methods
+    public void setA1(double a1){
+        this.a1 = a1;
+    }
+    
+    public void setA2(double a2){
+        this.a2 = a2;
+    }
+    
+    public double getA1(){
+        return a1;
+    }  
+    
+    public double getA2(){
+        return a2;
+    }
+    
+    public double getAverage(){
+        return average;
+    }
+    
+    public void calculateAverage(){
+        average = (a1 + 2*a2)/3;
+    }
+}
