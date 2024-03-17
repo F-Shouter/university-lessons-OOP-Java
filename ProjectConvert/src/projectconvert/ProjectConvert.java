1680df70033ceaefce2078d46872f983ba7216bd
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-03-17 16:49:06 -0300

    Code added - Float to String

--- ProjectConvert/src/projectconvert/ProjectConvert.java
+++ ProjectConvert/src/projectconvert/ProjectConvert.java
@@ -15,13 +15,19 @@ public class ProjectConvert {
      */
     public static void main(String[] args) {
         String st, st2;
-        Float real, real2;
+        float real, real2;
         int int1, int2;
         
         //String to Float
         st = "12";
         real = (float)(new Float(st).floatValue());
         System.out.println("String to Float " + real);
+        
+        //Float to String
+        real2 = 10;
+        st2 = new Float(real2).toString();
+        System.out.println("Float to String " + st2);
+
     }
     
 }
