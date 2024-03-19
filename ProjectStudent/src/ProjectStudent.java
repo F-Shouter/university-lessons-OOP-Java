547644d78fab5fca8f0e1c6552ee3b71988f0840
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-03-18 21:59:23 -0300

    correction in the comment stucture

--- ProjectStudent/src/ProjectStudent.java
+++ ProjectStudent/src/ProjectStudent.java
@@ -18,15 +18,16 @@ public class ProjectStudent {
         college.setA1(new Float(args[0]).floatValue());
         college.setA2(new Float(args[1]).floatValue());
         college.calculateAverage();
-        System.out.println("First semester grade: " + college.getA1());
+        System.out.println("First semester grade.: " + college.getA1());
         System.out.println("Second semester grade: " + college.getA2());
-        System.out.println("Final semester grade: " + college.getAverage());
+        System.out.println("Final semester grade.: " + college.getAverage());
     }
     
-    //Command prompt opened 
-    /*C:\Users\anney\OneDrive\Imagens\Documentos\GitHub_Projects\university-lessons-OOP-Java\ProjectStudent\build\classes>
-    //dir
-    //java ProjectStudent 5 5
-    //enter
+    /*Command prompt opened 
+    ProjectStudent\build\classes
+    dir
+    java ProjectStudent 5 5
+    enter
+    */
     
 }
