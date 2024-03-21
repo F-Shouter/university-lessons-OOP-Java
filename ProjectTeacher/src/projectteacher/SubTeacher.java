0119482bf67bd6ae0227b6780d9da7da643a1d62
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-03-20 23:00:04 -0300

    remmaining code added

--- ProjectTeacher/src/projectteacher/SubTeacher.java
+++ ProjectTeacher/src/projectteacher/SubTeacher.java
@@ -9,5 +9,43 @@ package projectteacher;
  * @author Fujoshi Shouter
  */
 public class SubTeacher {
+    private String name;
+    private double hours;
+    private double salary;
+    private static double valuePerHour = 50;
     
+    //constructor
+    public SubTeacher(String name, double hours){
+        this.name = name;
+        this.hours = hours;
+}
+    //acess methods
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setHours(double hours){
+        this.hours = hours;
+    }
+    public double getHours(){
+        return hours;
+    }
+    public double getSalary(){
+        return salary;
+    }
+    public void calculateSalary(){
+        salary = hours*SubTeacher.valuePerHour;
+    }
+    public double calculateIRPF(){
+        double aux;
+        if(salary<1800){
+            aux = 0.1*salary;
+        }
+        else{
+            aux = 0.27*salary;
+        }
+        return (aux);
+    }
 }
