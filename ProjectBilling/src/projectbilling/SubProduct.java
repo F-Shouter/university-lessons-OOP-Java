b3a43381aed7d2dea4d85e3fe79b2be4b5591b24
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-03-22 22:36:07 -0300

    Added code in SubProduct-Constructor and acess methods

--- ProjectBilling/src/projectbilling/SubProduct.java
+++ ProjectBilling/src/projectbilling/SubProduct.java
@@ -9,5 +9,37 @@ package projectbilling;
  * @author Fujoshi Shouter
  */
 public class SubProduct {
+    private int code;
+    private String description;
+    private double unitPrice;
+    private int quantilySold;
     
+    //constructor
+    public SubProduct(){}
+    
+    //acess methods
+    public void setCode(int code){
+        this.code = code;
+    }
+    public int getCode(){
+        return code;
+    } 
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setUnitPrice(double unitPrice){
+        this.unitPrice = unitPrice;
+    }
+    public double getUnitPrice(){
+        return unitPrice;
+    }
+    public void setQuantilySold(int quantilySold){
+        this.quantilySold = quantilySold;
+    }
+    public int getQuantilySold(){
+        return quantilySold;
+    } 
 }
