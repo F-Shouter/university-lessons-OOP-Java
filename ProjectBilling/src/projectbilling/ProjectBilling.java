d3bdea5a59cef260d2945f2f1e64487ee8e876c7
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-03-22 23:12:42 -0300

    Added code in SubProduct-remaining code

--- ProjectBilling/src/projectbilling/ProjectBilling.java
+++ ProjectBilling/src/projectbilling/ProjectBilling.java
@@ -8,13 +8,38 @@ package projectbilling;
  *
  * @author Fujoshi Shouter
  */
+import java.util.Scanner;
 public class ProjectBilling {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner output = new Scanner(System.in);
+        SubProduct prod = new SubProduct();
+        
+        System.out.print("Insert code: ");
+        int code = output.nextInt();
+        prod.setCode(code);
+        
+        output.nextLine();
+        
+        System.out.print("What's it? ");
+        String description = output.nextLine();
+        prod.setDescription(description);
+        
+        System.out.print("How much does it cust per unit?: ");
+        double unitPrice = output.nextDouble();
+        prod.setUnitPrice(unitPrice);
+        
+        System.out.print("How many " + prod.getDescription() +" do you want? ");
+        int quantilysold = output.nextInt();
+        prod.setQuantilySold(quantilysold);
+        
+        System.out.println("------------------------------------");
+        System.out.println("The Total: R$ " + prod.calculateTotal() + " reais.");
+        
+        output.close();
     }
     
 }
