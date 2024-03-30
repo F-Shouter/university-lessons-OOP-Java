2186445c14d0f66d573f940648020566e5b36635
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-03-30 18:56:36 -0300

    code added in  SubclassCar.java

--- ProjectCar/src/projectcar/SubclassCar.java
+++ ProjectCar/src/projectcar/SubclassCar.java
@@ -9,5 +9,16 @@ package projectcar;
  * @author Fujoshi Shouter
  */
 public class SubclassCar {
+    int year;
+    String brand;
+    String model;
     
+    public SubclassCar(int year, String brand, String model){
+        this.year = year;
+        this.brand = brand;
+        this.model = model;
+    }
+    public String toString(){
+        return "Car{" + "year=" + year + "brand=" + brand + "model=" + model + '}';
+    }
 }
