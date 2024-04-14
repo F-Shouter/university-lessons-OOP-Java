14305f65c2002e4b93d4055133dd4f4b5caf24fd
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-04-14 19:46:09 -0300

    added code in HourlyClass

--- ProjectPaper/src/projectpaper/Hourly.java
+++ ProjectPaper/src/projectpaper/Hourly.java
@@ -8,6 +8,21 @@ package projectpaper;
  *
  * @author Fujoshi Shouter
  */
-public class Hourly {
+public class Hourly extends Employee{
+    private double hours;
     
+    //Constructor
+    public Hourly(String name, String address, double hours){
+        super(name, address);
+        this.hours = hours;
+    }
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+    public double getHours() {
+        return hours;
+    }
+    public void calcularWage(){
+        wage = hours * 50;
+    }         
 }
