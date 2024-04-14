a65b3cf657d1cc9d7eac5668ba698a6c1017e71d
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-04-14 19:05:31 -0300

    corrected code

--- ProjectPaper/src/projectpaper/Employee.java
+++ ProjectPaper/src/projectpaper/Employee.java
@@ -3,10 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package projectpaper;
-
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  *
  * @author Fujoshi Shouter
