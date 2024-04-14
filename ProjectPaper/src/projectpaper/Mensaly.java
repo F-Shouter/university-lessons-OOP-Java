85e57e6f7a6795afe287f286e5731c718f94b17e
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-04-14 19:40:47 -0300

    added code in MensalyClass

--- ProjectPaper/src/projectpaper/Mensaly.java
+++ ProjectPaper/src/projectpaper/Mensaly.java
@@ -8,6 +8,34 @@ package projectpaper;
  *
  * @author Fujoshi Shouter
  */
-public class Mensaly {
+public class Mensaly extends Employee{
+    private String office;
     
+    //Constructor
+    public Mensaly(String name, String address, String office){
+        super(name, address);
+        this.office = office;
+    }
+    
+    //Acess methods
+    public void setOffice(String office) {
+        this.office = office;
+    }
+    public String getOffice() {
+        return office;
+    }
+    
+    public void calculateWage(){
+        if(office.equals("Junior")){
+            wage = 2500;
+        }
+        else{
+            if(office.equals("Pleno")){
+                wage = 3500;
+            }
+            else{
+                wage = 5500;
+            }
+        }
+    }
 }
