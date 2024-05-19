042d7fe2fb62d2da1d0e7a76df332035a493db9a
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-05-19 17:51:49 -0300

    Classe principal main Folha feito

--- Folha/src/folha/Folha.java
+++ Folha/src/folha/Folha.java
@@ -15,6 +15,24 @@ public class Folha {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Mensalista men = new Mensalista("Ricardo","Suzano-SP","Junior");
+        men.calcularSalario();
+       
+        System.out.println("Nome.........: " + men.getNome() + " de " + men.getEndereco());
+        System.out.println("Salario......: " + men.getSalario());
+        System.out.println("Imposto INSS.: " + men.calcularInss());
+        System.out.println("------------------------------------");
+        System.out.println("Salario FINAL: " + (men.getSalario() - men.calcularInss()));
+        
+        System.out.println();
+        Horista hor = new Horista("Anne","Mogi-SP",100);
+        hor.calcularSalario();
+        
+        System.out.println("Nome.........: " + hor.getNome() + " de " + hor.getEndereco());
+        System.out.println("Salario......: " + hor.getSalario());
+        System.out.println("Imposto IRPF.: " + hor.calcularIrpf());
+        System.out.println("------------------------------------");
+        System.out.println("Salario FINAL: " + (hor.getSalario() - hor.calcularIrpf()));
     }
     
 }
